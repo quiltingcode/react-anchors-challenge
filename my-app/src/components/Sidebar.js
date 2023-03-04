68f15a40dbd5dc42941da01ff99e14ef33bce4cf
Kelly Hutchison <kellyhutchison@hotmail.com>
2023-03-04 13:07:01 +0000

    Add sidebar component

---
+++ my-app/src/components/Sidebar.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Sidebar = () => {
+    return <div> 
+        <a>My Photos</a>
+        <a>My Illustrations </a>
+        <a>My Paintings</a>
+    </div>
+}
+
+
+
+export default Sidebar;
