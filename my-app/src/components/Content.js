a1185121abfc46a36d6ab389ee178dd500f7d4be
Kelly Hutchison <kellyhutchison@hotmail.com>
2023-03-11 11:11:20 +0000

    Add search filter component

--- my-app/src/components/Content.js
+++ my-app/src/components/Content.js
@@ -9,7 +9,8 @@ export class Content extends Component {
     constructor(props) {
       super(props)
       this.state = {
-         isLoaded: false
+         isLoaded: false,
+         posts: [],
       }
     }
 
@@ -17,9 +18,20 @@ export class Content extends Component {
         console.log('component mounted')
         setTimeout(() => {
           this.setState({
-            isLoaded: true
+            isLoaded: true,
+            posts: savedPosts,
           })
         }, 2000)
+      }
+
+      handleChange = (event) => {
+        const name = event.target.value.toLowerCase();
+        const filteredPosts = savedPosts.filter((post) => {
+          return post.name.toLowerCase().includes(name);
+      })
+        this.setState({
+          posts: filteredPosts
+        })
       }
 
   render() {
@@ -27,10 +39,21 @@ export class Content extends Component {
       <div className={css.Content} >
         <div className= {css.TitleBar}>
             <h1>My Photos</h1>
+            <form>
+              <label htmlFor="searchinput">Search:</label>
+              <input 
+                id="searchinput"
+                name="searchinput"
+                type="search"
+                placeholder="By Author"
+                onChange={(event) => this.handleChange(event)}
+              />
+              <h4>posts found: {this.state.posts.length}</h4>
+            </form>
         </div>
         <div className= {css.SearchResults}>
           {this.state.isLoaded ? (
-            <PostItem posts={savedPosts}/>
+            <PostItem savedPosts={this.state.posts}/>
           ) : (
             <Loader />
           )}
--- my-app/src/components/PostItem.js
+++ my-app/src/components/PostItem.js
@@ -1,21 +1,22 @@
-import React from 'react'
-import css from "./css/Content.module.css";
+
+import React from "react"
+import css from "./css/PostItem.module.css";
+
 
 function PostItem(props) {
-    const posts = props.posts
-  return (
-    posts.map((post) => {
-        const { name, title, image, description } = post;
-            return (
-                <div className={css.SearchItem} key={title}>
-                    <p>{title}</p>
-                    <p>{name}</p>
-                    <img src={image} alt="various images"></img>
-                    <p>{description}</p>
-                </div>
-            )
-        })
-    )};
-
+    return (
+        props.savedPosts.map(post => {
+            // Extra task destructuring 
+            const {title, name, image, description} = post
+            return <div className={css.SearchItem} key={title}>
+                <p>{title}</p>
+                <p>{name}</p>
+                <img src={image} alt="random"/>
+                <p>{description}</p>
+            </div>
+            }
+        )
+    )
+}
 
 export default PostItem
